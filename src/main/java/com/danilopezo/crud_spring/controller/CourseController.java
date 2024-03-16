54a751e5d5c121e64629729098bbdbd1ff8fdb17
Danilo Pezo <danilo_97pem@hotmail.com>
2024-03-16 15:07:43 -0300

    Criando o método post

--- src/main/java/com/danilopezo/crud_spring/controller/CourseController.java
+++ src/main/java/com/danilopezo/crud_spring/controller/CourseController.java
@@ -3,10 +3,10 @@ package com.danilopezo.crud_spring.controller;
 import com.danilopezo.crud_spring.model.Course;
 import com.danilopezo.crud_spring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +18,15 @@ public class CourseController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // @RequestBody: converte uma representação de recurso vinda de um cliente em um objeto.
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 }
--- src/main/java/com/danilopezo/crud_spring/model/Course.java
+++ src/main/java/com/danilopezo/crud_spring/model/Course.java
@@ -1,5 +1,6 @@
 package com.danilopezo.crud_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import lombok.Data;
 public class Course {
     @Id //chave primária
     @GeneratedValue(strategy = GenerationType.AUTO) //valor seja gerado automáticamente
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false) //nullable nao aceita valores nulos
